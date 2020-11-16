cfa1e346454f225933d593cfeb0da8f7fc39ee55
Sponsoredby <74147554+Sponsoredby@users.noreply.github.com>
2020-11-16 21:23:00 +0000

    Add files via upload

---
+++ euro to us floors.py
@@ -0,0 +1,5 @@
+#This converts european lift numbers to US numbers, I didnt even know this was a thing
+inp = input ("European Floor? ") #this asks the user to enter a floor number
+usf = int (inp) +1 # this converts the string for European Floor to an integer so we can add 1 to it 
+print ("US floor", usf) # displays the result
+# in the future come back and learn data validation 
